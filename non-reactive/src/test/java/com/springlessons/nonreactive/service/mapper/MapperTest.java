c89ad973cb2a993d5c5e35c3eac388c508ab3a8f
dasha <jjg-lessons@gmail.com>
2024-07-25 20:34:44 +0300

    +

---
+++ non-reactive/src/test/java/com/springlessons/nonreactive/service/mapper/MapperTest.java
@@ -0,0 +1,58 @@
+package com.springlessons.nonreactive.service.mapper;
+
+import com.springlessons.nonreactive.dto.author.Attachment;
+import com.springlessons.nonreactive.dto.author.AuthorFromOtherService;
+import com.springlessons.nonreactive.dto.book.AuthorForClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class MapperTest {
+    private Mapper mapper;
+
+    @BeforeEach
+    void init(){
+        mapper = new Mapper();
+    }
+
+    // public AuthorForClient mapAuthor(AuthorFromOtherService authorFromOtherService)
+    // new RuntimeException
+    @Test
+    void mapAuthor_ThrowsRuntimeException_NoMainPhoto() {
+        AuthorFromOtherService authorFromOtherService =
+                new AuthorFromOtherService(1, "author01", "about author01",
+                        List.of(
+                                new Attachment("mini_url", Attachment.AttachmentType.MINI_PHOTO),
+                                new Attachment("video_url", Attachment.AttachmentType.VIDEO)
+                        )
+                );
+
+        Assertions.assertThrows(
+                RuntimeException.class,
+                () -> mapper.mapAuthor(authorFromOtherService)
+        );
+    }
+
+    @Test
+    void mapAuthor_ReturnsAuthorForClient_ValidAuthorFromOS(){
+        Attachment mainPhoto =
+                new Attachment("main_url", Attachment.AttachmentType.MAIN_PHOTO);
+        AuthorFromOtherService authorFromOtherService =
+                new AuthorFromOtherService(1, "author01", "about author01",
+                        List.of(mainPhoto)
+                );
+
+        AuthorForClient authorForClient = mapper.mapAuthor(authorFromOtherService);
+
+        Assertions.assertNotNull(authorForClient);
+
+        Assertions.assertEquals(1, authorForClient.id());
+        Assertions.assertEquals("author01", authorForClient.name());
+        Assertions.assertEquals("main_url", authorForClient.photoUrl());
+
+    }
+}
