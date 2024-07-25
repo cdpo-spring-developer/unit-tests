31bea551a09a0653015b6b57a390d59d6b2aa134
dasha <jjg-lessons@gmail.com>
2024-07-25 19:41:04 +0300

    +

---
+++ non-reactive/src/test/java/com/springlessons/nonreactive/NonReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlessons.nonreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonReactiveApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
